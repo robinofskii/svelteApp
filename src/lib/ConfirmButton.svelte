49a93d964b5864ae94526bb5a6a59d224c8e8458
Robinofskii <rvandenberg@yieldcomputer.com>
2023-04-30 22:50:53 +0200

    feat: add confirmbutton
    This button will assume a clicked state on click and execute an action when clicked again in 1 second

---
+++ src/lib/ConfirmButton.svelte
@@ -0,0 +1,55 @@
+<script>
+  let clicked = false;
+
+  $: if (clicked) {
+    setTimeout(() => {
+      clicked = false;
+    }, 1000);
+  }
+
+  const handleConfirm = () => {
+    if (clicked) {
+      alert("Confirmed!")
+      return;
+    }
+    clicked = true;
+  }
+</script>
+
+<span on:click={handleConfirm} on:keydown={handleConfirm} class:clicked>
+  {#if clicked}
+    <p>Are you sure?</p>
+  {:else}
+    <p>Logout</p>
+  {/if}
+  
+</span>
+
+<style lang="scss">
+  @use "src/styles/variables" as *;
+  span {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    
+    cursor: pointer;
+    padding: $padding;
+    border-radius: $border-radius;
+    border: none;
+    background-color: $gray-light;
+
+    outline: solid 2px $primary-color;
+
+    p {
+      color: $text-color;
+      font-weight: bold;
+      font-size: 1rem;
+    }
+
+    &.clicked {
+      background-color: $primary-color;
+      color: $gray-light;
+    }
+  }
+</style>
