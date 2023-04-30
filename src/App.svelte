a985d57eac7a764d73348e4c7f4b4c511d59e453
Robinofskii <rvandenberg@yieldcomputer.com>
2023-04-30 23:17:34 +0200

    feat: use named slots to add icon to button

--- src/App.svelte
+++ src/App.svelte
@@ -1,139 +1,145 @@
 <script>
-  import Counter from './lib/Counter.svelte'
-  import Button from './lib/Button.svelte'
+	import Counter from './lib/Counter.svelte';
+	import Button from './lib/Button.svelte';
 	import ConfirmButton from './lib/ConfirmButton.svelte';
 	import Divider from './lib/Divider.svelte';
 
-  // JS of image
-  const imagedims = {
-    width: 500,
-    height: 250
-  }
-  const imageUrl = `https://picsum.photos/${imagedims.width}/${imagedims.height}`
-  const name = 'World'
+	import MdThumbUp from 'svelte-icons/md/MdThumbUp.svelte';
 
-  // JS of text
-  const htmlText = 'Here is some example text with some <em>HTML</em> tags'
+	// JS of image
+	const imagedims = {
+		width: 500,
+		height: 250,
+	};
+	const imageUrl = `https://picsum.photos/${imagedims.width}/${imagedims.height}`;
+	const name = 'World';
 
-  // JS of counter
-  let maxCount = 1
-  
-  // JS of button
-  let btnSize = 'medium'
+	// JS of text
+	const htmlText = 'Here is some example text with some <em>HTML</em> tags';
 
-  // JS of confirm button
-  const confirmButtonProps = {
-    timeOut: 5000,
-    onConfirm: () => {
-      alert("Impatient are we?")
-    },
-    btnText: "Do you want to wait 5 seconds?",
-    clickText: "Don't click me again!"
-  }
+	// JS of counter
+	let maxCount = 1;
+
+	// JS of button
+	let btnSize = 'medium';
+
+	// JS of confirm button
+	const confirmButtonProps = {
+		timeOut: 5000,
+		onConfirm: () => {
+			alert('Impatient are we?');
+		},
+		btnText: 'Do you want to wait 5 seconds?',
+		clickText: "Don't click me again!",
+	};
 </script>
 
 <section>
-  <img src={imageUrl} alt="an example placeholder">
-  <h1>Hello {name.toLocaleLowerCase()}</h1>
-  <p>{@html htmlText}</p>
-  <Divider />
-  <div class="maxInput">
-    <label for="maxCount">Max count:</label>
-    <input type="number" id="maxCount" bind:value={maxCount} min="1">
-  </div>
-  <div class="buttonRow">
-    <Counter initialCount={5} maxCount={maxCount}/>
-  </div>
-  <Divider />
-  <div class="buttonRow">
-    <Button size={btnSize} hasShadow>Other text</Button>
-  </div> 
-  <div class="buttonSizeSwitch">
-    <label for="btnSize">Button size:</label>
-    <select id="btnSize" bind:value={btnSize}>
-      <option value="small">Small</option>
-      <option value="medium">Medium</option>
-      <option value="large">Large</option>
-    </select>
-  </div>
-  <Divider />
-  <div class="logoutSection">
-    <ConfirmButton {...confirmButtonProps}/>
-  </div>
-
+	<img src={imageUrl} alt="an example placeholder" />
+	<h1>Hello {name.toLocaleLowerCase()}</h1>
+	<p>{@html htmlText}</p>
+	<Divider />
+	<div class="maxInput">
+		<label for="maxCount">Max count:</label>
+		<input type="number" id="maxCount" bind:value={maxCount} min="1" />
+	</div>
+	<div class="buttonRow">
+		<Counter initialCount={5} {maxCount} />
+	</div>
+	<Divider />
+	<div class="buttonRow">
+		<Button size={btnSize} hasShadow>
+			<span slot="leftContent">
+				<MdThumbUp />
+			</span>
+			Other text
+		</Button>
+	</div>
+	<div class="buttonSizeSwitch">
+		<label for="btnSize">Button size:</label>
+		<select id="btnSize" bind:value={btnSize}>
+			<option value="small">Small</option>
+			<option value="medium">Medium</option>
+			<option value="large">Large</option>
+		</select>
+	</div>
+	<Divider />
+	<div class="logoutSection">
+		<ConfirmButton {...confirmButtonProps} />
+	</div>
 </section>
 
 <style lang="scss">
-  @use "src/styles/variables" as *;
+	@use 'src/styles/variables' as *;
 
-  section {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 2rem;
-    padding: 2rem;
-    border-radius: $border-radius;
-  }
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 2rem;
+		padding: 2rem;
+		border-radius: $border-radius;
+	}
 
-  h1 {
-    color: $text-color;
-  }
-  img {
-    border-radius: $border-radius;
-  }
-  p {
-    color: $text-color;
-  }
-  .maxInput {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 1rem;
+	h1 {
+		color: $text-color;
+	}
+	img {
+		border-radius: $border-radius;
+	}
+	p {
+		color: $text-color;
+	}
+	.maxInput {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		gap: 1rem;
 
-    input {
-      padding: $padding;
-      border-radius: $border-radius;
-      border: none;
-      background-color: $gray-light;
-      color: $text-color;
-      font-weight: bold;
-      font-size: 1rem;
-      width: 10rem;
-      text-align: center;
-      &:focus {
-        outline: solid 2px $primary-color;
-      }
-    }
-  }
-  .buttonSizeSwitch {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 1rem;
-    select {
-      padding: $padding;
-      border-radius: $border-radius;
-      border: none;
-      background-color: $gray-light;
-      color: $text-color;
-      font-weight: bold;
-      font-size: 1rem;
-      width: 10rem;
-      text-align: center;
-      &:focus {
-        outline: solid 2px $primary-color;
-      }
-    }
-  }
-  .buttonRow {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 2rem;
-  }
+		input {
+			padding: $padding;
+			border-radius: $border-radius;
+			border: none;
+			background-color: $gray-light;
+			color: $text-color;
+			font-weight: bold;
+			font-size: 1rem;
+			width: 10rem;
+			text-align: center;
+			&:focus {
+				outline: solid 2px $primary-color;
+			}
+		}
+	}
+	.buttonSizeSwitch {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		gap: 1rem;
+		select {
+			padding: $padding;
+			border-radius: $border-radius;
+			border: none;
+			background-color: $gray-light;
+			color: $text-color;
+			font-weight: bold;
+			font-size: 1rem;
+			width: 10rem;
+			text-align: center;
+			&:focus {
+				outline: solid 2px $primary-color;
+			}
+		}
+	}
+	.buttonRow {
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+		gap: 2rem;
+	}
 
-  .logoutSection{
-    width: 10rem;
-  }
+	.logoutSection {
+		width: 10rem;
+	}
 </style>
--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,50 +1,90 @@
 <script>
-  export let size = 'medium';
-  export let hasShadow = false;
+	export let size = 'medium';
+	export let hasShadow = false;
 
-  const sizes = {
-    small: 'size-sm',
-    medium: 'size-md',
-    large: 'size-lg'
-  }
+	const sizes = {
+		small: 'size-sm',
+		medium: 'size-md',
+		large: 'size-lg',
+	};
+
+	console.log($$slots);
 </script>
 
-<button class={sizes[size]} class:shadow={hasShadow}><slot>Some text</slot></button>
+<button class={sizes[size]} class:shadow={hasShadow}>
+	{#if $$slots.leftContent}
+		<div class="left-content">
+			<slot name="leftContent" />
+		</div>
+	{/if}
+	<slot>Some text</slot>
+</button>
 
 <style lang="scss">
-  @use "src/styles/variables" as *;
-  button {
-    margin: 1em;
-    background-color: $primary-color;
-    color: $text-color;
-    font-weight: bold;
-    padding: $padding $padding-large;
-    border-radius: $border-radius;
-    border: none;
-    cursor: pointer;
-    &.size-sm {
-      padding: $padding $padding;
-      font-size: 0.8rem;
-    }
-    &.size-md {
-      padding: $padding $padding-large;
-      font-size: 1rem;
-    }
-    &.size-lg {
-      padding: $padding-large $padding-large*2;
-      font-size: 1.2rem;
-    }
-    &.shadow {
-      box-shadow: $box-shadow;
-    }
-    &:active {
-      color: $gray-light;
-      transition: background-color 0.3s $transition-cubic;
-      background-color: scale-color($color: $primary-color, $saturation: 0%, $lightness: -10%, $alpha: 0%);
+	@use 'src/styles/variables' as *;
+	button {
+		margin: 1em;
+		background-color: $primary-color;
+		color: $text-color;
+		font-weight: bold;
+		padding: $padding $padding-large;
+		border-radius: $border-radius;
+		border: none;
+		cursor: pointer;
 
-      &.shadow {
-        box-shadow: $box-shadow-inset;
-      }
-    }
-  }
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		.left-content {
+			max-width: 2rem;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+		}
+		&.size-sm {
+			padding: $padding $padding;
+			font-size: 0.8rem;
+			gap: 0.5rem;
+
+			.left-content {
+				max-width: 1rem;
+			}
+		}
+		&.size-md {
+			padding: $padding $padding-large;
+			font-size: 1rem;
+			gap: 1rem;
+
+			.left-content {
+				max-width: 2rem;
+			}
+		}
+		&.size-lg {
+			padding: $padding-large $padding-large * 2;
+			font-size: 1.2rem;
+			gap: 1.5rem;
+
+			.left-content {
+				max-width: 3rem;
+			}
+		}
+		&.shadow {
+			box-shadow: $box-shadow;
+		}
+		&:active {
+			color: $gray-light;
+			transition: background-color 0.3s $transition-cubic;
+			background-color: scale-color(
+				$color: $primary-color,
+				$saturation: 0%,
+				$lightness: -10%,
+				$alpha: 0%
+			);
+
+			&.shadow {
+				box-shadow: $box-shadow-inset;
+			}
+		}
+	}
 </style>
